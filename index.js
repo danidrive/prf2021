c2601a75feaa9ac27964e32720a4f5daab3d8ef5
Kaposvári Dániel <Kaposvari.Daniel@stud.u-szeged.hu>
2021-05-22 11:22:06 +0200

    MongoDB added

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
-
 const app = express();
 
+
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    console.log('Connected');
+    client.close().then(() => console.log('Closed'));
+});
+
+const port = parseInt(process.env.PORT, 10);
+
 app.get('/', ((req, res) => {
+    console.log('hello')
     res.send("Hello world!")
 }));
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
     console.log('The server is running!')
 })

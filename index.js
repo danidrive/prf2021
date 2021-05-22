2b569c0aa21614743c7c0f86d25a982f095cdd2c
Kaposvári Dániel <Kaposvari.Daniel@stud.u-szeged.hu>
2021-05-22 20:36:06 +0200

    Example model added to Mongo DB and get and post routes added.

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const mongodbUri = process.env.MONGODB_URI;
+const port = parseInt(process.env.PORT, 10);
 
-const MongoClient = require('mongodb').MongoClient;
+mongoose.connect(mongodbUri, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(
+        () => {
+            console.log('Successfully connected!')
+        })
+    .catch(
+        err => {
+            console.log('Connection failed: ', err)
+        })
 
-const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    console.log('Connected');
-    client.close().then(() => console.log('Closed'));
+mongoose.connection.on('error', err => {
+    console.log('Error occurred in DB: ', err)
 });
 
-const port = parseInt(process.env.PORT, 10);
+require('./models/example.model');
+
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({}));
 
 app.get('/', ((req, res) => {
     console.log('hello')
     res.send("Hello world!")
 }));
+
+app.use('/', require('./routes/example.route'));
+
+app.use((req, res) => {
+    console.log("Invalid request URL!")
+    res.status(404).send(
+        {
+            statusCode: 404,
+            requestUri: req.protocol + '://' + req.get('host') + req.originalUrl,
+            message: 'The requested resource not found.'
+        })
+})
 
 app.listen(port, () => {
     console.log('The server is running!')

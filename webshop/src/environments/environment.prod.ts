5a335791a4f0f3d35173060c8c8f7a0f3a4a7af5
Kaposvári Dániel <Kaposvari.Daniel@stud.u-szeged.hu>
2021-05-31 12:59:17 +0200

    node api url fix

--- webshop/src/environments/environment.prod.ts
+++ webshop/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  nodeApiUri: 'https://prf2021.herokuapp.com/api'
+  nodeApiUri: 'https://prf2021-webshop.herokuapp.com/api',
+  springApiUri: 'https://prf2021-transaction-api.herokuapp.com'
 };
--- webshop/src/environments/environment.ts
+++ webshop/src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   nodeApiUri: 'http://localhost:3000/api',
+  springApiUri: 'http://localhost:8080/'
 };
 
 /*
